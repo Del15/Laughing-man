db9ee48350a3e1d78be898bfc7a7ea593ffc9da6
Del15 <ellisderrick205@gmail.com>
2025-11-24 10:32:48 -0500

    boxes hit the border and bounce back, changes background color when hittting the border also!

--- LaughingMan.js
+++ LaughingMan.js
@@ -1,32 +1,54 @@
-var squareCount = 3;
+const SQUARE_COUNT = 10;
+const SPEED = 5;
+const TIMER_SPEED = 100;
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', ()=>{
     let squareElement = document.getElementById("square");
-    squareElement.addEventListener("click", function() {
-        alert("Square clicked!");
+    //let squareElement = document.querySelector("#square");
+    squareElement.addEventListener("click", ()=>{
+        alert("OMG YOU CAUGHT ME!");
     });
 
+    let box = document.querySelector("#box");
 
+    for(let i = 0;i<SQUARE_COUNT;i++)
+    {
+        //create the square
+        let square = document.createElement('img');
+        square.src = 'laughing_man.jpg';
+        square.alt = 'Catch the Laughing Man!';
+        square.className = 'square';
+        box.appendChild(square);
+        //add it to the screen   
+    }
+    let allSquares = box.children;
+    let squareArray = Array.from(allSquares);
+    squareArray.forEach(function (element, index) {
+        var dx = SPEED * Math.random() * 2 - 1;
+        var dy = SPEED * Math.random() * 2 - 1;
+        setInterval(() => {
+            let currentLeft = parseInt(element.style.left) || 225;
+            let currentTop = parseInt(element.style.top) || 175;
+            if (currentLeft >= 450 || currentLeft <= 0) {
+                dx *= -1;
+                element.style.borderColor = getRandomColor();
+            }
+            if (currentTop >= 350 || currentTop <= 0) {
+                dy *= -1;
+                element.style.borderColor = getRandomColor();
+            }
+            element.style.left = (currentLeft + dx) + "px";
+            element.style.top = (currentTop + dy) + "px";
+        }, TIMER_SPEED);
+    });
+    
+    
+});
 
-let box = document.querySelector("#box");
-
-for (let i = 0; i < squareCount; i++) {
-    let square = document.createElement("img");
-    square.src = "LaughingMan.jpg";
-    square.alt = "catch the laughing man";
-    square.className = "square";
-    box.appendChild(square);
-
-
-
+function getRandomColor() {
+    var r = parseInt(Math.random() * 256);
+    var g = parseInt(Math.random() * 256);
+    var b = parseInt(Math.random() * 256);
+    let color = "rgb(" + r + "," + g + "," + b + ")";
+    return color;
 }
-
-let allSquares = box.children;
-let squareArray = Array.from(children);
-squareArray.forEach(function(element,index){
-    var dx= SPEED * (Math.random()*2-1);
-    var dy= SPEED * (Math.random()*2-1);
-
-});
-
-});
